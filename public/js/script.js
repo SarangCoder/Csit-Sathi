baf4f337c7013182b02dd30bde98973b9db61d55
ankurAryal <aryalankur24@gmail.com>
2024-08-06 20:16:12 +0545

    first UI Completed

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+// This import is for navbar bars toggle on and off 
+
+import { Collapse, Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Collapse, Ripple });
+
+// -----------------------------------------------------------------------------------------
+
